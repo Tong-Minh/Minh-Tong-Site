81250f6a99f6c7263f1ba0c7fc12699bf58131de
Minh Tong <minhtong972@gmail.com>
2025-01-05 20:54:26 -0600

    title change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import Navbar from "./components/navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
