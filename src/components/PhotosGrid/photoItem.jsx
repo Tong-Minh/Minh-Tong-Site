69388a58e289958b384856a314779e6923053c10
Minh Tong <minhtong972@gmail.com>
2025-01-09 18:07:06 -0600

    lazy loading

--- src/components/PhotosGrid/photoItem.jsx
+++ src/components/PhotosGrid/photoItem.jsx
@@ -7,7 +7,7 @@ const photoItem = (props) => {
     return (
         <PhotoView src={props.img}>
             <div className={isVertical ? 'row-span-2 bg-[#e2e2e2] p-4 rounded-sm h-full' : 'bg-[#e2e2e2] p-4 rounded-sm h-full'}>
-                <img src={props.img} alt={props.alt} className="w-full h-full object-cover rounded-sm shadow-inner" />
+                <img src={props.img} alt={props.alt} loading="lazy" className="w-full h-full object-cover rounded-sm shadow-inner" />
             </div>
         </PhotoView>
     )
